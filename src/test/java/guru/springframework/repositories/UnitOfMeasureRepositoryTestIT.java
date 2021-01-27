58dc639507555a5c2ad5585948ec35a25519cb9e
michaelarayabe <michaelseifuaraya@gmail.com>
2021-01-27 12:23:09 +0100

    added MockMvc test and unit test

---
+++ src/test/java/guru/springframework/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -0,0 +1,17 @@
+package guru.springframework.repositories;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitOfMeasureRepositoryTestIT {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByDescription() {
+    }
+}
