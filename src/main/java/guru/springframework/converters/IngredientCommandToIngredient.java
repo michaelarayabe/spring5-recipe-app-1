00b4fe0b8b49d66b590a82debc406811faeafe7c
michaelarayabe <michaelseifuaraya@gmail.com>
2021-02-02 14:07:13 +0100

    update ingriedent

--- src/main/java/guru/springframework/converters/IngredientCommandToIngredient.java
+++ src/main/java/guru/springframework/converters/IngredientCommandToIngredient.java
@@ -2,6 +2,7 @@ package guru.springframework.converters;
 
 import guru.springframework.commands.IngredientCommand;
 import guru.springframework.domain.Ingredient;
+import guru.springframework.domain.Recipe;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
@@ -15,16 +16,23 @@ public class IngredientCommandToIngredient implements Converter<IngredientComman
         this.uomConverter = uomConverter;
     }
 
-
     @Nullable
     @Override
     public Ingredient convert(IngredientCommand source) {
-        if(source == null) {
+        if (source == null) {
             return null;
         }
 
         final Ingredient ingredient = new Ingredient();
         ingredient.setId(source.getId());
+
+        if(source.getRecipeId() != null){
+            Recipe recipe = new Recipe();
+            recipe.setId(source.getRecipeId());
+            ingredient.setRecipe(recipe);
+            recipe.addIngredient(ingredient);
+        }
+
         ingredient.setAmount(source.getAmount());
         ingredient.setDescription(source.getDescription());
         ingredient.setUom(uomConverter.convert(source.getUom()));
